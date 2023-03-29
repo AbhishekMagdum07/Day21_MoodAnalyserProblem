f361ee09efbfea9805a0473593000ce51f3db54e
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-29 19:19:56 +0530

    Added : use custom exception for invalid input

---
+++ src/main/java/com/mood_analyzer/MoodAnalysisException.java
@@ -0,0 +1,7 @@
+package com.mood_analyzer;
+
+public class MoodAnalysisException extends RuntimeException{
+    public MoodAnalysisException(String msg) { // creating custom exception.
+        super(msg);
+    }
+}
